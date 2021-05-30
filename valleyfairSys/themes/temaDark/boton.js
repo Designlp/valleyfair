379dcc3a336b91ae5eefcbf6fc121f9e06543d26
Design <dmercadoff@gmail.com>
2021-05-30 17:41:01 -0400

    boton css

--- valleyfairSys/themes/temaDark/boton.js
+++
@@ -1,8 +0,0 @@
-const loginButton = document.querySelector('.login-button');
-loginButton.onmousemove = function(e){
-    const x = e.pageX - loginButton.offsetLeft;
-    const y = e.pageY- loginButton.offsetTop;
-
-    loginButton.style.setProperty('--x', x + 'px');
-    loginButton.style.setProperty('--y', y + 'px');
-}
