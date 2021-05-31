bf50c578b1f870e1030670898da35e863dae7ae2
Design <dmercadoff@gmail.com>
2021-05-30 23:10:44 -0400

    header

--- valleyfairSys/themes/temaDark/js/boton.js
+++ valleyfairSys/themes/temaDark/js/boton.js
@@ -1,5 +1,5 @@
 const topbarButton = document.querySelector('.topbar-button');
-const buttonbeau = document.querySelector('.button-beau');
+// const buttonbeau = document.querySelector('.button-beau');
 topbarButton.onmousemove = function(e){
     const x = e.pageX - topbarButton.offsetLeft;
     const y = e.pageY- topbarButton.offsetTop;
@@ -8,10 +8,10 @@ topbarButton.onmousemove = function(e){
     topbarButton.style.setProperty('--y', y + 'px');
 }
 
-buttonbeau.onmousemove = function(e){
-    const xx = e.pageX - buttonbeau.offsetLeft;
-    const yy = e.pageY- buttonbeau.offsetTop;
+// buttonbeau.onmousemove = function(e){
+//     const xx = e.pageX - buttonbeau.offsetLeft;
+//     const yy = e.pageY- buttonbeau.offsetTop;
 
-    buttonbeau.style.setProperty('--xx', x + 'px');
-    buttonbeau.style.setProperty('--yy', y + 'px');
-}
+//     buttonbeau.style.setProperty('--xx', x + 'px');
+//     buttonbeau.style.setProperty('--yy', y + 'px');
+// }
