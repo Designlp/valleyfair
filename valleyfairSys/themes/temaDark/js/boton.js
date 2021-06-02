0776101741c7e5615c2b46882b916ab4190e0e60
Design <dmercadoff@gmail.com>
2021-06-01 23:17:02 -0400

    botones

--- valleyfairSys/themes/temaDark/js/boton.js
+++ valleyfairSys/themes/temaDark/js/boton.js
@@ -1,19 +1,19 @@
-// const topbarButton = document.querySelector('.topbar-button');
+const topbarButton = document.querySelector('.topbar-button');
 
-// topbarButton.onmousemove = function(e){
-//     const x = e.pageX - topbarButton.offsetLeft;
-//     const y = e.pageY- topbarButton.offsetTop;
+topbarButton.onmousemove = function(e){
+    const x = e.pageX - topbarButton.offsetLeft;
+    const y = e.pageY- topbarButton.offsetTop;
 
-//     topbarButton.style.setProperty('--x', x + 'px');
-//     topbarButton.style.setProperty('--y', y + 'px');
-// }
-
-
-const botonJS = document.querySelector('.botonJS');
-botonJS.onmousemove = function(e){
-    const x = e.pageX - botonJS.offsetLeft;
-    const y = e.pageY- botonJS.offsetTop;
-
-    botonJS.style.setProperty('--x', x + 'px');
-    botonJS.style.setProperty('--y', y + 'px');
+    topbarButton.style.setProperty('--x', x + 'px');
+    topbarButton.style.setProperty('--y', y + 'px');
 }
+
+
+// const botonJS = document.querySelector('.botonJS');
+// botonJS.onmousemove = function(e){
+//     const x = e.pageX - botonJS.offsetLeft;
+//     const y = e.pageY- botonJS.offsetTop;
+
+//     botonJS.style.setProperty('--x', x + 'px');
+//     botonJS.style.setProperty('--y', y + 'px');
+// }
