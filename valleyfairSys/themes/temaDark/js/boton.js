bdba22f722c067c6d82816335fcaf9ad2810b4d2
Design <dmercadoff@gmail.com>
2021-06-01 22:22:17 -0400

    botones

--- valleyfairSys/themes/temaDark/js/boton.js
+++ valleyfairSys/themes/temaDark/js/boton.js
@@ -1,19 +1,19 @@
-const topbarButton = document.querySelector('.topbar-button');
+// const topbarButton = document.querySelector('.topbar-button');
 
-topbarButton.onmousemove = function(e){
-    const x = e.pageX - topbarButton.offsetLeft;
-    const y = e.pageY- topbarButton.offsetTop;
+// topbarButton.onmousemove = function(e){
+//     const x = e.pageX - topbarButton.offsetLeft;
+//     const y = e.pageY- topbarButton.offsetTop;
 
-    topbarButton.style.setProperty('--x', x + 'px');
-    topbarButton.style.setProperty('--y', y + 'px');
-}
+//     topbarButton.style.setProperty('--x', x + 'px');
+//     topbarButton.style.setProperty('--y', y + 'px');
+// }
 
 const botonJS = document.querySelector('.botonJS');
 
 botonJS.onmousemove = function(e){
-    const xxx = e.pageX - botonJS.offsetLeft;
-    const yyy = e.pageY- botonJS.offsetTop;
+    const x = e.pageX - botonJS.offsetLeft;
+    const y = e.pageY- botonJS.offsetTop;
 
-    botonJS.style.setProperty('--xxx', xxx + 'px');
-    botonJS.style.setProperty('--yyy', yyy + 'px');
+    botonJS.style.setProperty('--x', x + 'px');
+    botonJS.style.setProperty('--y', y + 'px');
 }
