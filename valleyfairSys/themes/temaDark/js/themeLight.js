eb76f6b9c32e70cfc9de882dc48025328506d432
Design <dmercadoff@gmail.com>
2021-06-04 22:14:57 -0400

    boton theme

--- valleyfairSys/themes/temaDark/js/themeLight.js
+++ valleyfairSys/themes/temaDark/js/themeLight.js
@@ -6,13 +6,15 @@ function cambiarColor(){
         document.documentElement.style.setProperty('--background', '#121519');
         document.documentElement.style.setProperty('--backgroundVentana', '#1A1E23');
         document.documentElement.style.setProperty('--colorWhiteM', '#D7D7D7');
+        document.documentElement.style.setProperty('--backgroundSearch', '#1F252D');
     }
     else{
         //day
         modificarColor(change);
         document.documentElement.style.setProperty('--background', '#EDF2F7');
         document.documentElement.style.setProperty('--backgroundVentana', '#F7FAFC');
-        document.documentElement.style.setProperty('--colorWhiteM', '#4D4D4D');
+        document.documentElement.style.setProperty('--colorWhiteM', '#787878');
+        document.documentElement.style.setProperty('--backgroundSearch', '#ebebeb');
     }
 }
 
