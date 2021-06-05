54eccf284ae4d4fc5523d45fcd5779623768f4c3
Design <dmercadoff@gmail.com>
2021-06-04 22:37:07 -0400

    boton theme

--- valleyfairSys/themes/temaDark/js/themeLight.js
+++ valleyfairSys/themes/temaDark/js/themeLight.js
@@ -8,15 +8,19 @@ function cambiarColor(){
         document.documentElement.style.setProperty('--colorWhiteM', '#D7D7D7');
         document.documentElement.style.setProperty('--colorIndicadores', '#999');
         document.documentElement.style.setProperty('--backgroundSearch', '#1F252D');
+        document.documentElement.style.setProperty('--backgroundhover', '#fdfdfd');
+        document.documentElement.style.setProperty('--backgroundAnimate', '#1A1E23');
     }
     else{
-        //day
+        //Mod
         modificarColor(change);
         document.documentElement.style.setProperty('--background', '#EDF2F7');
         document.documentElement.style.setProperty('--backgroundVentana', '#F7FAFC');
-        document.documentElement.style.setProperty('--colorWhiteM', '#ffffff');//787878
+        document.documentElement.style.setProperty('--colorWhiteM', '#5C5C5C');//787878
         document.documentElement.style.setProperty('--colorIndicadores', '#BDBDBD');
         document.documentElement.style.setProperty('--backgroundSearch', '#ebebeb');
+        document.documentElement.style.setProperty('--backgroundhover', '#3b3b3b');
+        document.documentElement.style.setProperty('--backgroundAnimate', '#64c1ff');
     }
 }
 
@@ -34,17 +38,4 @@ function modificarColor(change) {
         }
     }
 
-    // if(change)
-    // {
-    //     var elements = document.getElementById('content::before'); // get all elements
-    //     for(var i = 0; i < elements.length; i++){
-    //         elements[i].style.backgroundColor = "var(--backgroundVentana)";
-    //     }
-    // }else{
-    //     var elements = document.getElementById('content::before'); // get all elements
-    //     for(var i = 0; i < elements.length; i++){
-    //         elements[i].style.backgroundColor = "var(--colorBefore)";
-    //     }
-    // }
-    //#content::before
 }
