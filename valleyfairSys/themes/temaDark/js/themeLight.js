b7d7203f86aaeea86f5ff961ab5a10c6af93bcce
Design <dmercadoff@gmail.com>
2021-06-04 21:33:36 -0400

    responsive

--- valleyfairSys/themes/temaDark/js/themeLight.js
+++ valleyfairSys/themes/temaDark/js/themeLight.js
@@ -3,26 +3,26 @@ function cambiarColor(){
     change=!change;
     if(change){
         modificarColor(change);
-        document.documentElement.style.setProperty('--main-day-color', '#317EEB');
+        document.documentElement.style.setProperty('--background', '#121519');
     }
     else{
         //day
         modificarColor(change);
-        document.documentElement.style.setProperty('--main-day-color', '#ff00ff');
+        document.documentElement.style.setProperty('--background', '#EDF2F7');
     }
 }
 
-function modificarColor(change) {
-    if(change)
-    {
-        var elements = document.getElementsByClassName('prueba'); // get all elements
-        for(var i = 0; i < elements.length; i++){
-            elements[i].style.backgroundColor = "var(--main-day-color)";
-        }
-    }else{
-        var elements = document.getElementsByClassName('prueba'); // get all elements
-        for(var i = 0; i < elements.length; i++){
-            elements[i].style.backgroundColor = "var(--main-nigth-color)";
-        }
-    }
-}
+// function modificarColor(change) {
+//     if(change)
+//     {
+//         var elements = document.getElementsByClassName('prueba'); // get all elements
+//         for(var i = 0; i < elements.length; i++){
+//             elements[i].style.backgroundColor = "var(--main-day-color)";
+//         }
+//     }else{
+//         var elements = document.getElementsByClassName('prueba'); // get all elements
+//         for(var i = 0; i < elements.length; i++){
+//             elements[i].style.backgroundColor = "var(--main-nigth-color)";
+//         }
+//     }
+// }
