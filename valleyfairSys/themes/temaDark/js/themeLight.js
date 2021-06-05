497fd9ebeee030881dee665ade5c60ebe2018b59
Design <dmercadoff@gmail.com>
2021-06-04 23:02:43 -0400

    boton theme

--- valleyfairSys/themes/temaDark/js/themeLight.js
+++ valleyfairSys/themes/temaDark/js/themeLight.js
@@ -10,6 +10,7 @@ function cambiarColor(){
         document.documentElement.style.setProperty('--backgroundSearch', '#1F252D');
         document.documentElement.style.setProperty('--backgroundhover', '#fdfdfd');
         document.documentElement.style.setProperty('--backgroundAnimate', '#1A1E23');
+        document.documentElement.style.setProperty('--imagesComentSVG', "url('images/icons/commentD.svg')");
     }
     else{
         //Mod
@@ -21,6 +22,7 @@ function cambiarColor(){
         document.documentElement.style.setProperty('--backgroundSearch', '#ebebeb');
         document.documentElement.style.setProperty('--backgroundhover', '#3b3b3b');
         document.documentElement.style.setProperty('--backgroundAnimate', '#64c1ff');
+        document.documentElement.style.setProperty('--imagesComentSVG', "url('images/icons/commentDark.svg')");
     }
 }
 
@@ -40,17 +42,17 @@ function modificarColor(change) {
 
 }
 
-function changeIconComment(change) {
-    if(change)
-    {
-        var elements = document.getElementsByClassName('commentD'); // get all elements
-        for(var i = 0; i < elements.length; i++){
-            elements[i].style.backgroundImage = "url('images/icons/commentD.svg')";
-        }
-    }else{
-        var elements = document.getElementsByClassName('topbar'); // get all elements
-        for(var i = 0; i < elements.length; i++){
-            elements[i].style.backgroundImage = "url('images/icons/commentD.svg')";
-        }
-    }
-}
+// function changeIconComment(change) {
+//     if(change)
+//     {
+//         var elements = document.getElementsByClassName('commentD'); // light
+//         for(var i = 0; i < elements.length; i++){
+//             elements[i].style.backgroundImage = "url('images/icons/commentD.svg')";
+//         }
+//     }else{
+//         var elements = document.getElementsByClassName('commentD'); // dark
+//         for(var i = 0; i < elements.length; i++){
+//             elements[i].style.backgroundImage = "url('images/icons/commentD.svg')";
+//         }
+//     }
+// }
