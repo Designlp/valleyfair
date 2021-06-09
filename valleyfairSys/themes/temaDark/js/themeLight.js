404eadf5a01c325d0f4c19ac3bcb84981cdaa71f
Design <dmercadoff@gmail.com>
2021-06-08 20:29:34 -0400

    terminando detalles css

--- valleyfairSys/themes/temaDark/js/themeLight.js
+++ valleyfairSys/themes/temaDark/js/themeLight.js
@@ -1,8 +1,54 @@
-let change= true;
+let change;
+let changer = document.getElementById('changer');
+window.onload=load();
+function load(){
+    if(!localStorage.getItem('change')){
+        localStorage.setItem('change','false');
+    }else{
+        change = localStorage.getItem('change');
+        if(change === 'true'){
+            changer.checked = true;
+            modificarColor(change);
+            document.documentElement.style.setProperty('--background', '#EDF2F7');
+            document.documentElement.style.setProperty('--backgroundVentana', '#F7FAFC');
+            document.documentElement.style.setProperty('--colorWhiteM', '#5C5C5C');//787878
+            document.documentElement.style.setProperty('--colorIndicadores', '#B5B5B5');
+            document.documentElement.style.setProperty('--backgroundSearch', '#ebebeb');
+            document.documentElement.style.setProperty('--backgroundhover', '#3b3b3b');
+            document.documentElement.style.setProperty('--backgroundAnimate', '#64c1ff');
+            document.documentElement.style.setProperty('--imagesComentSVG', "url('images/icons/commentDark.svg')");
+            document.documentElement.style.setProperty('--imagesInactiveLikeSVG', "url('images/icons/heart-inactiveDark.svg')");
+            document.documentElement.style.setProperty('--imagesSharedSVG', "url('images/icons/shareDDark.svg')");
+            document.documentElement.style.setProperty('--backgroundletrasamigos', '#3b3b3b');
+            document.documentElement.style.setProperty('--colorNotifications', '#ffffff');
+            document.documentElement.style.setProperty('--color1Notifications', '#149FFE');
+        }
+        else{
+            modificarColor(change);
+            changer.checked = false;
+            document.documentElement.style.setProperty('--background', '#15202B');
+            document.documentElement.style.setProperty('--backgroundVentana', '#192734');
+            document.documentElement.style.setProperty('--colorWhiteM', '#D7D7D7');
+            document.documentElement.style.setProperty('--colorIndicadores', '#999');
+            document.documentElement.style.setProperty('--backgroundSearch', '#213343');
+            document.documentElement.style.setProperty('--backgroundhover', '#fdfdfd');
+            document.documentElement.style.setProperty('--backgroundAnimate', '#1B2937');
+            document.documentElement.style.setProperty('--imagesComentSVG', "url('images/icons/commentD.svg')");
+            document.documentElement.style.setProperty('--imagesInactiveLikeSVG', "url('images/icons/heart-inactive.svg')");
+            document.documentElement.style.setProperty('--imagesSharedSVG', "url('images/icons/shareD.svg')");
+            document.documentElement.style.setProperty('--backgroundletrasamigos', '#f8f8f8');
+            document.documentElement.style.setProperty('--colorNotifications', '#149FFE');
+            document.documentElement.style.setProperty('--color1Notifications', '#15202B');
+        }
+        console.log(change);
+    }
+}
+
 function cambiarColor(){
-    change=!change;
-    if(change){
+    change = localStorage.getItem('change');
+    if(change === 'true'){
         modificarColor(change);
+        changer.checked = false;
         document.documentElement.style.setProperty('--background', '#15202B');
         document.documentElement.style.setProperty('--backgroundVentana', '#192734');
         document.documentElement.style.setProperty('--colorWhiteM', '#D7D7D7');
@@ -16,12 +62,14 @@ function cambiarColor(){
         document.documentElement.style.setProperty('--backgroundletrasamigos', '#f8f8f8');
         document.documentElement.style.setProperty('--colorNotifications', '#149FFE');
         document.documentElement.style.setProperty('--color1Notifications', '#15202B');
+        localStorage.setItem('change','false');
     }
 
     //B5B5B5
 
     else{
         //Mod
+        switcher.checked = true;
         modificarColor(change);
         document.documentElement.style.setProperty('--background', '#EDF2F7');
         document.documentElement.style.setProperty('--backgroundVentana', '#F7FAFC');
@@ -36,11 +84,12 @@ function cambiarColor(){
         document.documentElement.style.setProperty('--backgroundletrasamigos', '#3b3b3b');
         document.documentElement.style.setProperty('--colorNotifications', '#ffffff');
         document.documentElement.style.setProperty('--color1Notifications', '#149FFE');
+        localStorage.setItem('change','true');
     }
 }
 
 function modificarColor(change) {
-    if(change)
+    if(change === 'true')
     {
         var elements = document.getElementsByClassName('topbar'); // get all elements
         for(var i = 0; i < elements.length; i++){
